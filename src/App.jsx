ba34abd48695b33939b4a8fc7c3e478fbebc4ae6
gdsabarna353 <gdsabarna353@gmail.com>
2026-01-22 12:12:47 +0530

    feat: added initial layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Header } from './components/header/Header'
+import { Main } from './components/main/Main'
+import { Sidebar } from './components/sidebar/Sidebar'
+import './App.css';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='app'>
+      <div className='right'>
+        <Header />
+        <Main />
+      </div>
+      <div className='left'>
+        <Sidebar />
+      </div>
+    </div>
   )
 }
 
